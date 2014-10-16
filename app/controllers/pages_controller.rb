a4dbd5798a72aec99c3ead48122d8a024653aed5
Bobby LE <bobby.weiss.le@gmail.com>
2014-10-16 18:32:13 +0200

    debts added on page index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
   def index
   	@user = current_user
+  	@debts = @user.debt.all
+  	#They owe you
+  	@theyoweyous = Debt.where(user_id: current_user.id, youtothem: false)
+
+  	#You owe them
+  	@youowethems = Debt.where(user_id: current_user.id, youtothem: true)
   end
   def about
   end
